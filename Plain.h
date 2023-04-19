9c900785221f5335aab4d82cc6d991c6bd63f7b6
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2023-04-19 23:55:02 +0900

    Change API from class to namespace

--- Plain.h
+++ Plain.h
@@ -10,201 +10,110 @@
 #include "Core/PLARendererType.hpp"
 #include "Core/PLAFunctionCode.hpp"
 
-class Plain {
-public:
+namespace Plain
+{
   using Renderer = PLARendererType;
 
-  static void Init(PLARendererType aType);
-  static void Delete(const std::string &aName);
+  void Init(PLARendererType aType);
+  void Delete(const std::string &aName);
 
-  static void Add(const PLAAGTActor &aActor);
-  static void Add(const PLAAGTModel &aModel);
+  void Add(const PLAAGTActor &aActor);
+  void Add(const PLAAGTModel &aModel);
 
-  static void Push(const PLAAGTScene &aAgent);
+  void Push(const PLAAGTScene &aAgent);
 
-  class Event {
-  public:
+  namespace Event
+  {
     using Actor = PLAFunctionCode::Actor;
     using Stage = PLAFunctionCode::Stage;
     using Scene = PLAFunctionCode::Scene;
-
-    /// Delete copy and move constructors and assign operators
-    Event(const Event&) = delete;
-    Event& operator=(const Event&) = delete;
-    Event(Event&&) = delete;
-    Event& operator=(Event&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    Event() = delete;
-    ~Event() = delete;
   };
 
-  class Error {
-  public:
+  namespace Error
+  {
     using Type = PLAErrorType;
-    static void Assert(const std::string &aMessage);
-    static void Expect(const std::string &aMessage);
 
-    /// Delete copy and move constructors and assign operators
-    Error(const Error&) = delete;
-    Error& operator=(const Error&) = delete;
-    Error(Error&&) = delete;
-    Error& operator=(Error&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    Error() = delete;
-    ~Error() = delete;
+    void Assert(const std::string &aMessage);
+    void Expect(const std::string &aMessage);
   };
 
-  class Print {
-  public:
-    static void Objects();
-    static void Resources();
-
-    /// Delete copy and move constructors and assign operators
-    Print(const Print&) = delete;
-    Print& operator=(const Print&) = delete;
-    Print(Print&&) = delete;
-    Print& operator=(Print&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    Print() = delete;
-    ~Print() = delete;
+  namespace Print
+  {
+    void Objects();
+    void Resources();
   };
 
-  class State {
-  public:
-    static PLAAGTState Assign();
-
-    /// Delete copy and move constructors and assign operators
-    State(const State&) = delete;
-    State& operator=(const State&) = delete;
-    State(State&&) = delete;
-    State& operator=(State&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    State() = delete;
-    ~State() = delete;
+  namespace State
+  {
+    PLAAGTState Assign();
   };
 
-  class Stage {
-  public:
-    static PLAAGTStage Assign();
-    static void SetFunction(Event::Stage aEventCode);
-
-    /// Delete copy and move constructors and assign operators
-    Stage(const Stage&) = delete;
-    Stage& operator=(const Stage&) = delete;
-    Stage(Stage&&) = delete;
-    Stage& operator=(Stage&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    Stage() = delete;
-    ~Stage() = delete;
+  namespace Stage
+  {
+    PLAAGTStage Assign();
+    void SetFunction(Event::Stage aEventCode);
   };
 
-  class Scene {
-  public:
-    static PLAAGTScene Create();
-    static PLAAGTScene Create(const PLAString &aName);
-    static PLAAGTScene Assign(const PLAString &aName);
-
-    /// Delete copy and move constructors and assign operators
-    Scene(const Scene&) = delete;
-    Scene& operator=(const Scene&) = delete;
-    Scene(Scene&&) = delete;
-    Scene& operator=(Scene&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    Scene() = delete;
-    ~Scene() = delete;
+  namespace Scene
+  {
+    PLAAGTScene Create();
+    PLAAGTScene Create(const PLAString &aName);
+    PLAAGTScene Assign(const PLAString &aName);
   };
 
-  class Model {
-  public:
-    static PLAAGTModel Create();
-    static PLAAGTModel Create(const PLAString &aName);
-    static PLAAGTModel Assign(const PLAString &aName);
-
-    /// Delete copy and move constructors and assign operators
-    Model(const Model&) = delete;
-    Model& operator=(const Model&) = delete;
-    Model(Model&&) = delete;
-    Model& operator=(Model&&) = delete;
-
-  private:
-    /// Delete default constructor and destructor
-    Model() = delete;
-    ~Model() = delete;
+  namespace Model
+  {
+    PLAAGTModel Create();
+    PLAAGTModel Create(const PLAString &aName);
+    PLAAGTModel Assign(const PLAString &aName);
   };
 
-  class Actor {
-  public:
-    static PLAAGTActor CreateRect(const PLAVec3f &aPivot,
-                                  const PLAColor &aColor,
-                                  const PLATransform &aTransform,
-                                  const PLARect &aRect);
-    static PLAAGTActor CreateRect(const PLAVec3f &aPivot,
-                                  const PLAColor &aColor,
-                                  const PLATransform &aTransform,
-                                  const PLARect &aRect,
-                                  const PLAColor &aFillColor);
-    static PLAAGTActor CreateRect(const PLAVec3f &aPivot,
-                                  const PLAColor &aColor,
-                                  const PLATransform &aTransform,
-                                  const PLARect &aRect,
-                                  const std::string &aImage,
-                                  const PLARect &aClip);
-    static PLAAGTActor CreateCircle(const PLAVec3f &aPivot,
-                                    const PLAColor &aColor,
-                                    const PLATransform &aTransform,
-                                    const PLACircle &aCircle);
-    static PLAAGTActor CreateCircle(const PLAVec3f &aPivot,
-                                    const PLAColor &aColor,
-                                    const PLATransform &aTransform,
-                                    const PLACircle &aCircle,
-                                    const PLAColor &aFillColor);
-    static PLAAGTActor CreateCircle(const PLAVec3f &aPivot,
-                                    const PLAColor &aColor,
-                                    const PLATransform &aTransform,
-                                    const PLACircle &aCircle,
-                                    const std::string &aImage,
-                                    const PLARect &aClip);
-    static PLAAGTActor CreateTile(const PLAVec2f &aOffset,
-                                  const std::string &aImageName,
-                                  const GRAVec2<PLASize> &aTileSize,
-                                  const GRAVec2<PLASize> &aChipSize,
-                                  const IPLATileLayerDataSource *aDataSource);
-    static PLAAGTActor Assign(const PLAString &aName);
+  namespace Actor
+  {
+    PLAAGTActor CreateRect(const PLAVec3f &aPivot,
+                           const PLAColor &aColor,
+                           const PLATransform &aTransform,
+                           const PLARect &aRect);
 
-    /// Delete copy and move constructors and assign operators
-    Actor(const Actor&) = delete;
-    Actor& operator=(const Actor&) = delete;
-    Actor(Plain&&) = delete;
-    Actor& operator=(Actor&&) = delete;
+    PLAAGTActor CreateRect(const PLAVec3f &aPivot,
+                           const PLAColor &aColor,
+                           const PLATransform &aTransform,
+                           const PLARect &aRect,
+                           const PLAColor &aFillColor);
 
-  private:
-    /// Delete default constructor and destructor
-    Actor() = delete;
-    ~Actor() = delete;
+    PLAAGTActor CreateRect(const PLAVec3f &aPivot,
+                           const PLAColor &aColor,
+                           const PLATransform &aTransform,
+                           const PLARect &aRect,
+                           const std::string &aImage,
+                           const PLARect &aClip);
+
+    PLAAGTActor CreateCircle(const PLAVec3f &aPivot,
+                             const PLAColor &aColor,
+                             const PLATransform &aTransform,
+                             const PLACircle &aCircle);
+
+    PLAAGTActor CreateCircle(const PLAVec3f &aPivot,
+                             const PLAColor &aColor,
+                             const PLATransform &aTransform,
+                             const PLACircle &aCircle,
+                             const PLAColor &aFillColor);
+
+    PLAAGTActor CreateCircle(const PLAVec3f &aPivot,
+                             const PLAColor &aColor,
+                             const PLATransform &aTransform,
+                             const PLACircle &aCircle,
+                             const std::string &aImage,
+                             const PLARect &aClip);
+
+    PLAAGTActor CreateTile(const PLAVec2f &aOffset,
+                           const std::string &aImageName,
+                           const GRAVec2<PLASize> &aTileSize,
+                           const GRAVec2<PLASize> &aChipSize,
+                           const IPLATileLayerDataSource *aDataSource);
+
+    PLAAGTActor Assign(const PLAString &aName);
   };
-
-  /// Delete copy and move constructors and assign operators
-  Plain(const Plain&) = delete;
-  Plain& operator=(const Plain&) = delete;
-  Plain(Plain&&) = delete;
-  Plain& operator=(Plain&&) = delete;
-
-private:
-  /// Delete default constructor and destructor
-  Plain() = delete;
-  ~Plain() = delete;
 };
 
 #endif //PLAIN_ENGINE_PLAIN_HPP
