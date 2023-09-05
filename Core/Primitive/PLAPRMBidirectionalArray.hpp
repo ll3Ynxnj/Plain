2519b7c49d878829de39850736a637461a1e2914
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2023-09-05 19:09:50 +0900

    Add bidirectional array

---
+++ Core/Primitive/PLAPRMBidirectionalArray.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2023. CLAYWORK Inc. All rights reserved.
+
+//
+// Created by Kentaro Kawai on 2023/09/01.
+//
+
+#ifndef ANHR_PLAPRMBIDIRECTIONALARRAY_HPP
+#define ANHR_PLAPRMBIDIRECTIONALARRAY_HPP
+
+#include "Grain/Primitive/GRAPRMBidirectionalArray.hpp"
+
+template <typename T>
+using PLABidirectionalArray = GRAPRMBidirectionalArray<T>;
+
+#endif //ANHR_PLAPRMBIDIRECTIONALARRAY_HPP
