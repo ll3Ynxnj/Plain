563bb2462d945013efaea0543bdce587f5bd5cca
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2025-12-27 18:32:45 +0900

    Add intermediate color constants (Orange, Lime, SpringGreen, Azure, Violet, Rose)

--- include/plain/core/primitive/PLAPRMColor.hpp
+++ include/plain/core/primitive/PLAPRMColor.hpp
@@ -8,6 +8,7 @@ using PLAColor = GRAColorRGBA8888<PLAFloat>;
 static const PLAColor kPLAColorNone        = PLAColor(0);
 static const PLAColor kPLAColorNorm        = PLAColor(1);
 static const PLAColor kPLAColorTransparent = PLAColor(1.00, 1.00, 1.00, 0.00);
+
 static const PLAColor kPLAColorBlack       = PLAColor(0.00, 0.00, 0.00, 1.00);
 static const PLAColor kPLAColorRed         = PLAColor(1.00, 0.00, 0.00, 1.00);
 static const PLAColor kPLAColorGreen       = PLAColor(0.00, 1.00, 0.00, 1.00);
@@ -17,5 +18,12 @@ static const PLAColor kPLAColorCyan        = PLAColor(0.00, 1.00, 1.00, 1.00);
 static const PLAColor kPLAColorMagenta     = PLAColor(1.00, 0.00, 1.00, 1.00);
 static const PLAColor kPLAColorGray        = PLAColor(0.50, 0.50, 0.50, 1.00);
 static const PLAColor kPLAColorWhite       = PLAColor(1.00, 1.00, 1.00, 1.00);
+
+static const PLAColor kPLAColorOrange      = PLAColor(1.00, 0.50, 0.00, 1.00);
+static const PLAColor kPLAColorSpringGreen = PLAColor(0.00, 1.00, 0.50, 1.00);
+static const PLAColor kPLAColorViolet      = PLAColor(0.50, 0.00, 1.00, 1.00);
+static const PLAColor kPLAColorLime        = PLAColor(0.50, 1.00, 0.00, 1.00);
+static const PLAColor kPLAColorAzure       = PLAColor(0.00, 0.50, 1.00, 1.00);
+static const PLAColor kPLAColorRose        = PLAColor(1.00, 0.00, 0.50, 1.00);
 
 #endif //PLAIN_PLACOLOR_HPP
