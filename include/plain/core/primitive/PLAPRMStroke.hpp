036ef18675ea86799c69abe9ed8d64bc5ef9d23f
Kentaro Kawai <12520747+ll3Ynxnj@users.noreply.github.com>
2025-12-27 18:19:49 +0900

    Add PLAPRMColor include and kPLAStrokeNorm constant

--- include/plain/core/primitive/PLAPRMStroke.hpp
+++ include/plain/core/primitive/PLAPRMStroke.hpp
@@ -2,10 +2,11 @@
 #define PLAIN_PLASTROKE_HPP
 
 #include "plain/core/primitive/PLAPRMType.hpp"
+#include "plain/core/primitive/PLAPRMColor.hpp"
 #include "grain/primitive/GRAPRMStroke.hpp"
 
 using PLAStroke = GRAStroke<PLAFloat>;
-
 static const PLAStroke kPLAStrokeNone = PLAStroke();
+static const PLAStroke kPLAStrokeNorm = PLAStroke(kPLAColorGray, 1.0f);
 
 #endif //PLAIN_PLASTROKE_HPP
